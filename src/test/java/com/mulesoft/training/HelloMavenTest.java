685dfbf73b4c4126cd79eb5ee7b4773eae55cae9
Mamta Chauhan <mamta.chauhan@sixtree.com.au>
2016-10-18 10:03:20 +1100

    Added new changes

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -16,13 +16,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     
     // retrieveFlights response contains content-type header
     // content-type headers has a value of application/json
- /*   @Test
+    @Test
     public void retrieveFlightsAddsAppropriateHeader() throws Exception{
     	MuleEvent event = runFlow("retrieveFlights");
     	String contentType = event.getMessage().getOutboundProperty("Content-Type");
     	assertEquals("application/json", contentType);
     }
-   */
+   
     // run maven-config.xml when testing
     @Override
     protected String getConfigFile() {
